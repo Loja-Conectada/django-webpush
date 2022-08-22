9c659ae123e0268951d4be3cb0e315ec55f1e1fb
ph_santosjr <paulo.henrique@databrain.com.br>
2022-08-22 11:01:01 -0300

    change migrations

--- webpush/migrations/0001_initial.py
+++ webpush/migrations/0001_initial.py
@@ -8,7 +8,8 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        #migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('dao', '0001_initital')
     ]
 
     operations = [

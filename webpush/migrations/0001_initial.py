5b245adbb6ded9bfa209ded6afc3bb83a30a902c
ph_santosjr <paulo.henrique@databrain.com.br>
2022-08-22 11:05:22 -0300

    change migrations

--- webpush/migrations/0001_initial.py
+++ webpush/migrations/0001_initial.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         #migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('dao', '0001_initital')
+        ('dao', '0001_initial')
     ]
 
     operations = [
